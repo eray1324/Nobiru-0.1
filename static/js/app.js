f83daee703a647aa338d18cd9e64f301d80e6d7c
eray1324 <yaretzimorenomartinez1@gmail.com>
2026-06-12 12:26:05 -0600

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -1,1 +1,22 @@
+const frases = [
 
+"El esfuerzo de hoy es el éxito de mañana.",
+
+"Cada pregunta resuelta te acerca a tu meta.",
+
+"Aprender es avanzar.",
+
+"La constancia supera al talento.",
+
+"Nunca subestimes una hora de estudio.",
+
+"Tu futuro comienza con lo que haces hoy.",
+
+"La disciplina vence a la motivación."
+
+];
+
+const numeroDia = new Date().getDay();
+
+document.getElementById("frase").innerText =
+frases[numeroDia];
