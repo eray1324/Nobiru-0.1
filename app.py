edf1153816cfd4a5f99ae907b6104eadf8b277f8
eray1324 <yaretzimorenomartinez1@gmail.com>
2026-06-13 14:09:04 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, session
 import sqlite3
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 app.secret_key = "nobiru_secret_key"
